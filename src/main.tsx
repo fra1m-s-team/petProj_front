49bebd8f3c8d73ea194095e579eb943bd7869fd8
vasili <frugas@list.ru>
2024-06-18 17:16:38 +0300

    del game

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { MantineProvider } from '@mantine/core';
 import LoginPage from './auth';
-import { initThreeJS } from './scripts/scripts';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import App from './components/app';
 import Store from './store/store';
-import { createContext, useEffect, useRef, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+
 interface IStore {
 	store: Store;
 }
@@ -20,30 +20,6 @@ const loadStyles = async () => {
 };
 
 loadStyles();
-
-//FIXME: Нужно сделать так, чтобы если опредленные данные ввести (log: admin, pass: admin)
-//FIXME: переводил ссылку на игру Apps которая начинается с 26 строки (пока игры показывается, если в 69 строке прописать apps)
-const Apps: React.FC = () => {
-	const threeContainerRef = useRef<HTMLDivElement>(null);
-
-	useEffect(() => {
-		if (threeContainerRef.current) {
-			initThreeJS(threeContainerRef.current);
-		}
-	}, []);
-
-	return (
-		<MantineProvider>
-			<div id='three-container' ref={threeContainerRef}></div>
-		</MantineProvider>
-	);
-};
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-// 	<MantineProvider>
-// 		<Apps />
-// 	</MantineProvider>
-// );
 
 function AppInit() {
 	const [isReady, setIsReady] = useState(false);
